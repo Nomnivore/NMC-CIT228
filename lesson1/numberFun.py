2343b05e54b1ad281f4ccb0a0c4e3b1203455080
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-01-17 12:06:10 -0500

    lesson 1

---
+++ lesson1/numberFun.py
@@ -0,0 +1,30 @@
+
+import random
+import this
+
+
+num1 = random.randrange(1, 100)
+num2 = random.randrange(1, 100)
+
+print("First number = ", num1)
+print("Second number = ", num2)
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} // {num2} = {num1 // num2}")
+print(f"{num1} % {num2} = {num1 % num2}")
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
+
+NUMBER2 = 25
+num2 = NUMBER2
+
+print("First number = ", num1)
+print("Second number = ", num2)
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} // {num2} = {num1 // num2}")
+print(f"{num1} % {num2} = {num1 % num2}")
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
+
+print(this)
---
+++ lesson1/stringFun.py
@@ -0,0 +1,38 @@
+# exercise 1
+print("----------------------")
+print("Exercise 1\n")
+name = "kyle warner"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My first initial is: ", name[0].upper())
+
+# exercise 2
+print("----------------------")
+print("Exercise 2\n")
+noun = "cat"
+adj = "chonky"
+verb = "scratched"
+ending = "all the furniture"
+sentence1 = "the " + adj + " " + noun + " " + verb + " " + ending
+sentence2 = f"the {adj} {noun} {verb} {ending}"
+print(sentence1.upper())
+print(sentence2.lower())
+
+# exercise 3
+print("----------------------")
+print("Exercise 3\n")
+quote = ("Mercy is most empowering, liberating, and transformative when it is "
+         "directed at the undeserving. The people who haven't earned it, "
+         "who haven't even sought it, are the most meaningful recipients"
+         " of our compassion.")
+cite = "Bryan Stevenson"
+
+print(f"{quote}\n-- {cite}")
+
+# exercise 4
+print("----------------------")
+print("Exercise 4\n")
+print("Breakfast\t\tBiscuit")
+print("Lunch\t\t\tPizza")
+print("Dinner\t\t\tPasta")
